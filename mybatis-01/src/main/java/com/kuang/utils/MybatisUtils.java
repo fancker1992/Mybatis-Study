007217790108973cc7363e4a6a8aa235d5e3051d
xiongda <786087292@qq.com>
2021-01-07 16:49:41 +0800

    首次提交

---
+++ mybatis-01/src/main/java/com/kuang/utils/MybatisUtils.java
@@ -0,0 +1,32 @@
+package com.kuang.utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MybatisUtils {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    static {
+        try {
+            //创建sqlSessionFactory  工厂类
+            String resource = "mybatis-config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //从工厂类中获取SqlSession 对象
+    public static SqlSession getSqlSession() {
+
+        return sqlSessionFactory.openSession();
+    }
+
+
+}
---
+++ mybatis-01/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -0,0 +1,30 @@
+package com.kuang.dao;
+
+import com.kuang.pojo.User;
+import com.kuang.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTest {
+
+    @Test
+    public void test() {
+        //获取session对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //执行sql    getMapper
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        List<User> userList = userDao.getUserList();
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        //关闭sqlSession
+        sqlSession.close();
+
+    }
+
+
+}
