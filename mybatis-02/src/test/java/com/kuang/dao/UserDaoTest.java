8ad74b9bbe876aa3f4e938612ac2c265032381fb
xiongda <786087292@qq.com>
2021-01-14 20:15:32 +0800

    新增mybatis-02模块 数据库配置读取db.properties 配置文件

---
+++ mybatis-02/src/test/java/com/kuang/dao/UserDaoTest.java
@@ -0,0 +1,108 @@
+package com.kuang.dao;
+
+import com.kuang.pojo.User;
+import com.kuang.utils.MybatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class UserDaoTest {
+    //获取用户列表
+    @Test
+    public void test() {
+        //获取session对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //执行sql    getMapper方式
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        List<User> userList = userDao.getUserList();
+
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        //关闭sqlSession
+        sqlSession.close();
+    }
+
+    //根据id进行查询
+    @Test
+    public void getUserById() {
+
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        User user = userDao.getUserById(7);
+        System.out.println(user);
+        sqlSession.close();
+
+    }
+
+    //模糊查询
+    @Test
+    public void getUserLike() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MybatisUtils.getSqlSession();
+            UserDao userDao = sqlSession.getMapper(UserDao.class);
+
+            List<User> userList = userDao.getUserLike("老");
+            for (User user : userList) {
+                System.out.println(user);
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+
+    //添加用户
+    @Test
+    public void addUser() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        try {
+
+            userDao.addUser(new User("老二", "女"));
+            sqlSession.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    //更新信息
+    @Test
+    public void updataUser() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+        try {
+
+            userDao.updataUser(7);
+            sqlSession.commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sqlSession.close();
+        }
+    }
+
+    //删除数据
+    @Test
+    public void deleteUser() {
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        UserDao userDao = sqlSession.getMapper(UserDao.class);
+
+        userDao.deleteUser(9);
+        sqlSession.commit();
+        sqlSession.close();
+    }
+
+
+}
