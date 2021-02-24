3721e5f0244ffae099ef65c9cea9f55f2e0e7b17
xiongda <786087292@qq.com>
2021-02-24 10:11:08 +0800

    06   新增多表关联查询,通过resultMap映射

--- mybatis-06/src/main/java/com/kuang/dao/StudentMapper.java
+++ mybatis-06/src/main/java/com/kuang/dao/StudentMapper.java
@@ -1,5 +1,11 @@
 package com.kuang.dao;
 
+import com.kuang.pojo.Student;
+
+import java.util.List;
+
 public interface StudentMapper {
 
+
+    public List<Student> getStudent();
 }
--- mybatis-06/src/test/java/com/kuang/dao/MyTest.java
+++ mybatis-06/src/test/java/com/kuang/dao/MyTest.java
@@ -1,5 +1,6 @@
 package com.kuang.dao;
 
+import com.kuang.pojo.Student;
 import com.kuang.pojo.Teacher;
 import com.kuang.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
@@ -12,7 +13,7 @@ public class MyTest {
     static Logger logger = Logger.getLogger(MyTest.class);
 
 
-    //获取用户列表   打印日志版
+    //获取teacher列表   打印日志版
     @Test
     public void getTeacher() {
         //获取session对象
@@ -26,4 +27,22 @@ public class MyTest {
         //关闭sqlSession
         sqlSession.close();
     }
+
+    @Test
+    public void getUserAndTeacher() {
+        //获取session对象
+        SqlSession sqlSession = MybatisUtils.getSqlSession();
+        //执行sql    getMapper方式
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        List<Student> students = mapper.getStudent();
+        for (Student student : students) {
+
+            logger.info(student);
+        }
+
+        //关闭sqlSession
+        sqlSession.close();
+    }
+
+
 }
